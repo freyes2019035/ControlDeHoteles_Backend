01d70fbd2e32d783f61601e2c3ad025080ac5808
freyes2019035 <Daniel_reyes2003@outlook.com>
2021-05-24 15:34:16 -0600

    user methods added

--- src/controllers/users/users.controller.js
+++ src/controllers/users/users.controller.js
@@ -31,6 +31,19 @@ exports.getUser = (req, res) => {
         }
     })
 }
+exports.getUserById = (req, res) => {
+    const userID = req.params.id;
+    userModel.findById(userID, (err, user) => {
+        if(err){
+            warning.message_500(res)
+        }else if(!user){
+            console.log(user)
+            warning.message_500(res)
+        }else{
+            res.status(200).send(user)
+        }
+    })
+}
 exports.updateUser = (req, res) => {
     let body = req.body;
     let userID = req.user.sub;
@@ -61,6 +74,33 @@ exports.updateUser = (req, res) => {
         warning.message_400(res)
     }
 }
+exports.updateUserById = (req, res) => {
+    let body = req.body;
+    let userID = req.params.id;
+    if(body && userID){
+        userModel.find({_id: userID}, (err, docFound) => {
+            if(err){
+                warning.message_500(res);
+            }else if(docFound && docFound.length >=1){
+                console.log(docFound[0].rol)
+                    delete body.rol
+                    userModel.findByIdAndUpdate(userID, body,{new: true},(err, docUpdate) => {
+                        if(err){
+                            warning.message_500(res)
+                        }else if(!docUpdate){
+                            warning.message_404(res, 'user')
+                        }else{
+                            res.status(200).send({userUpdated: docUpdate})
+                        }
+                    });
+            }else{
+                warning.message_404(res, 'the user')
+            }
+        })
+    }else{
+        warning.message_400(res)
+    }
+}
 exports.deleteUser = (req, res) => {
     let userID = req.user.sub;
     if(userID){
@@ -81,6 +121,30 @@ exports.deleteUser = (req, res) => {
                 }else{
                     warning.message_custom(res, 'You can not delete a user that is not yours, STATUS 401')
                 }
+            }else{
+                warning.message_404(res, 'the user')
+            }
+        })
+    }else{
+        warning.message_400(res)
+    }
+} 
+exports.deleteUserById = (req, res) => {
+    let userID = req.params.id;
+    if(userID){
+        userModel.find({_id: userID}, (err, docFound) => {
+            if(err){
+                warning.message_500(res);
+            }else if(docFound && docFound.length >=1){
+                    userModel.findByIdAndRemove(userID, (err, docRemoved) => {
+                        if(err){
+                            warning.message_500(res)
+                        }else if(!docRemoved){
+                            warning.message_404(res, 'user')
+                        }else{
+                            res.status(200).send([{status: 200}, {userRemoved: docRemoved}])
+                        }
+                    });
             }else{
                 warning.message_404(res, 'the user')
             }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,7 +3,10 @@ const router = require('express').Router()
 const mdAuth = require('../middlewares/auth.middleware');
 
 router.put('/updateAccount', mdAuth.ensureAuth ,userController.updateUser);
+router.put('/updateAccountById/:id', mdAuth.ensureAuth ,userController.updateUserById);
 router.delete('/deleteAccount', mdAuth.ensureAuth ,userController.deleteUser);
+router.delete('/deleteAccountById/:id', mdAuth.ensureAuth ,userController.deleteUserById);
 router.get('/getUsers', mdAuth.ensureAuth, userController.getUsers)
 router.get('/getUser', mdAuth.ensureAuth, userController.getUser)
+router.get('/getUserById/:id', mdAuth.ensureAuth, userController.getUserById)
 module.exports = router;
